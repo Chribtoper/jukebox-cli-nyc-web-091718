c65cc5d354c2d638f892390d75e5b95c463f5c20
Chribtoper <xcstolfax@gmail.com>
2018-09-13 02:39:41 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -37,7 +37,17 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-
+  puts "Please enter a song name or number:"
+    input = gets.chomp
+    songs.each_with_index do |song, idx|
+      if input.to_i == (idx+1) || input == song
+        puts "Playing #{song}"
+        break
+      else
+        puts "Invalid input, please try again"
+        break
+      end
+     end
 end
 
 def exit_jukebox
